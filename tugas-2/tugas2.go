eec222e6f8411933dc597aefa434de92815f51fc
Administrator <administrator@LEL-ID-L-M-0002.local>
2023-12-12 03:02:48 +0700

    Tugas-2 belum selesai

---
+++ tugas-2/tugas2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	//Soal 2
+
+	halo := "Halo Duia"
+	halo = " Halo Golang"
+
+	fmt.Println(halo)
+
+	//Soal 3
+
+	var kataPertama = "saya"
+	var kataKedua = "senang"
+	var kataKetiga = "belajar"
+	var kataKeempat = "golang"
+
+	fmt.Println(kataPertama + " " + kataKedua + " " + kataKetiga + " " + kataKeempat)
+
+	//Soal 4
+
+	var angkaPertama = 8
+	var angkaKedua = 5
+	var angkaKetiga = 6
+	var angkaKeempat = 7
+
+	fmt.Println(angkaPertama + angkaKedua + angkaKetiga + angkaKeempat)
+
+	//Soal 5
+	kalimat := "halo halo bandung"
+	angka := 2021
+
+	// fmt.Println(kalimat + "- + angka)
+}
